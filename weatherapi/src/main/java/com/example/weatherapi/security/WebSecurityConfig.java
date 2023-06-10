c9481a81c2ae315bfeb5fcd5f1f2e9100a389d69
knottem <59744487+knottem@users.noreply.github.com>
2023-06-10 20:07:30 +0200

    small updates to properties

--- weatherapi/src/main/java/com/example/weatherapi/security/WebSecurityConfig.java
+++ weatherapi/src/main/java/com/example/weatherapi/security/WebSecurityConfig.java
@@ -1,17 +1,27 @@
 package com.example.weatherapi.security;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -26,10 +36,11 @@ public class WebSecurityConfig {
     @Value("${spring.security.user.password}")
     private String password;
 
+    //Setting up security for the endpoints.
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        //Setting roles
+        //Setting roles for endpoints, nothing should be accessible without a role.
         http.authorizeHttpRequests(r -> r
                 .requestMatchers("/weather/**").hasRole("USER")
                 .requestMatchers("/city/**").hasRole("USER"));
@@ -40,6 +51,7 @@ public class WebSecurityConfig {
         return http.build();
     }
 
+    //Setting up a user with a role. More users can be added here, but for now we only need one.
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.withUsername(username)
@@ -49,8 +61,10 @@ public class WebSecurityConfig {
         return new InMemoryUserDetailsManager(user);
     }
 
+    //Using BCrypt for password encoding.
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }

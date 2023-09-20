2bf11bafc8c14dbc9135287f3b2c730569d4be8a
Erik <59744487+knottem@users.noreply.github.com>
2023-09-20 17:40:43 +0200

    Added more data for testing and started writing tests for deleting cities in the db

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -3,10 +3,18 @@ INSERT INTO `city` (`lat`, `lon`, `name`)VALUES
     (57.7075,11.9675,'Göteborg'), -- 2
     (55.6058,13.0358,'Malmö'), -- 3
     (59.8581,17.6447,'Uppsala'), -- 4
-    (58.6,16.2,'Norrköping'), -- 5
+    (58.6,16.2,'Norrköping'), -- 5 - Used for delete test
     (59.8601,17.64,'Uppsala'), -- 6
     (59.6161,16.5528,'Västerås'), -- 7
-    (59.2739,15.2075,'Örebro'); -- 8
+    (59.2739,15.2075,'Örebro'), -- 8
+    (58.4158,15.6253,'Linköping'),
+    (56.05,12.7167,'Helsingborg'),
+    (57.7828,14.1606,'Jönköping'),
+    (57.6667,15.85,'Vimmerby'),
+    (62.4,17.3167,'Sundsvall'),
+    (60.6747,17.1417,'Gävle'),
+    (63.825,20.2639,'Umeå'),
+    (64.75,20.95,'Skellefteå');
 
 INSERT INTO `auth` (`username`, `password`, `role`)VALUES
     ('admin','$2a$10$Eq3yxcRlqmIaxBv7w9qkDuZCfw7lhgYS12ndADouoUogkNMWGmELK', 'ADMIN' ), -- pass123 for testing

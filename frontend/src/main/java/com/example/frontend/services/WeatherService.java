e319bc75cb71214fcd19a6f1bd3641e9df623200
Erik <59744487+knottem@users.noreply.github.com>
2023-09-20 13:03:39 +0200

    Removed dbcity and frontend to make sure this repo focus on the backend, will update the readme soon with links to the other repos for frontend and the db or they might stay private and just use packages in the docker-compose

--- frontend/src/main/java/com/example/frontend/services/WeatherService.java
+++
@@ -1,4 +0,0 @@
-package com.example.frontend.services;
-
-public interface WeatherService {
-}
--- frontend/src/main/java/com/example/frontend/services/impl/WeatherServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.frontend.services.impl;
-
-import com.example.frontend.services.WeatherService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class WeatherServiceImpl implements WeatherService {
-}

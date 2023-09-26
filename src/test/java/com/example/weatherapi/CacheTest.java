f0b9acb3b0ce91ac94b0de2be137c7c34516b594
Erik <59744487+knottem@users.noreply.github.com>
2023-09-26 10:00:25 +0200

    Changed test methods name in CacheTest

--- src/test/java/com/example/weatherapi/CacheTest.java
+++ src/test/java/com/example/weatherapi/CacheTest.java
@@ -10,8 +10,6 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
 public class CacheTest {
 
     @AfterEach
@@ -21,7 +19,7 @@ public class CacheTest {
 
     // Test Case 1: Check that the cache we get is the same as the one we put in
     @Test
-    public void getWeatherFromCacheTest_Valid(){
+    public void shouldRetrieveValidWeatherFromCache(){
         Weather weather = Weather.builder().build();
         Cache.getInstance().put("key", weather);
         assertEquals(weather, Cache.getInstance().getWeatherFromCache("key", 1));
@@ -29,7 +27,7 @@ public class CacheTest {
 
     // Test Case 2: Check that the cache is expired
     @Test
-    public void getWeatherFromCacheTest_Expired(){
+    public void shouldDetectExpiredWeatherInCache(){
         Weather weather = Weather.builder().build();
         Cache.getInstance().put("key", weather);
         assertNull(Cache.getInstance().getWeatherFromCache("key", -1));
@@ -37,7 +35,7 @@ public class CacheTest {
 
     // Test Case 3: Check that the cache doesn't exist
     @Test
-    public void getWeatherFromCacheTest_DoesntExist() {
+    public void shouldHandleNonExistentWeatherInCache() {
         assertNull(Cache.getInstance().getWeatherFromCache("key", 1));
     }
 }

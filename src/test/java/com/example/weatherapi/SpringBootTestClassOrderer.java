f50988de9226f313e84d421f055462506f36ce26
Erik <59744487+knottem@users.noreply.github.com>
2023-09-30 16:49:35 +0200

    Added Custom order for tests just to make sure unit tests runs first then the rest will run

---
+++ src/test/java/com/example/weatherapi/SpringBootTestClassOrderer.java
@@ -0,0 +1,34 @@
+package com.example.weatherapi;
+
+import org.junit.jupiter.api.ClassDescriptor;
+import org.junit.jupiter.api.ClassOrderer;
+import org.junit.jupiter.api.ClassOrdererContext;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Comparator;
+
+// This class is used to order the tests in the order we want them to run.
+// We use this to make sure that we run unit tests before integration tests since if unit tests fail,
+// we don't want to wait for the integration tests to run.
+
+// From: https://howtodoinjava.com/junit5/test-execution-order/
+public class SpringBootTestClassOrderer implements ClassOrderer {
+    @Override
+    public void orderClasses(ClassOrdererContext classOrdererContext) {
+        classOrdererContext.getClassDescriptors().sort(Comparator.comparingInt(SpringBootTestClassOrderer::getOrder));
+    }
+
+    private static int getOrder(ClassDescriptor classDescriptor) {
+        if (classDescriptor.findAnnotation(SpringBootTest.class).isPresent()) {
+            return 4;
+        } else if (classDescriptor.findAnnotation(WebMvcTest.class).isPresent()) {
+            return 3;
+        } else if (classDescriptor.findAnnotation(DataJpaTest.class).isPresent()) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
+}

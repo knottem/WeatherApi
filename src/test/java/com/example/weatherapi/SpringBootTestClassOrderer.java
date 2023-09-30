7b1e5112fda81389ebb643e5367a2db8352563f9
Erik <59744487+knottem@users.noreply.github.com>
2023-09-30 18:10:16 +0200

    Added docs to authmapper and SpringBootTestClassOrderer

--- src/test/java/com/example/weatherapi/SpringBootTestClassOrderer.java
+++ src/test/java/com/example/weatherapi/SpringBootTestClassOrderer.java
@@ -9,17 +9,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Comparator;
 
-// This class is used to order the tests in the order we want them to run.
-// We use this to make sure that we run unit tests before integration tests since if unit tests fail,
-// we don't want to wait for the integration tests to run.
-
-// From: https://howtodoinjava.com/junit5/test-execution-order/
+/**
+ * This class is used to order the tests in the order we want them to run.
+ * We use this to make sure that we run unit tests before integration tests since if unit tests fail,
+ * we don't want to wait for the integration tests to run.
+ * <p>
+ * From: <a href="https://howtodoinjava.com/junit5/test-execution-order/">https://howtodoinjava.com/junit5/test-execution-order/</a>
+ */
 public class SpringBootTestClassOrderer implements ClassOrderer {
+
+    /**
+     * Order the supplied {@code ClassDescriptor ClassDescriptors} and return a new list
+     * @param classOrdererContext the {@code ClassOrdererContext} containing the
+     * {@linkplain ClassDescriptor class descriptors} to order; never {@code null}
+     */
     @Override
     public void orderClasses(ClassOrdererContext classOrdererContext) {
         classOrdererContext.getClassDescriptors().sort(Comparator.comparingInt(SpringBootTestClassOrderer::getOrder));
     }
 
+    /**
+     * Get the order of the test class, with the highest order being run first
+     * @param classDescriptor the {@code ClassDescriptor} to get the order for
+     * @return the order of the test class
+     */
     private static int getOrder(ClassDescriptor classDescriptor) {
         if (classDescriptor.findAnnotation(SpringBootTest.class).isPresent()) {
             return 4;

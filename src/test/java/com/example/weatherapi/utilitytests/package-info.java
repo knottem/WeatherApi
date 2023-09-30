e8dbebe3d3879ca482832f9f88cbceb76336e017
Erik <59744487+knottem@users.noreply.github.com>
2023-09-30 14:44:06 +0200

    Added more javadocs and testing adding a package-info

---
+++ src/test/java/com/example/weatherapi/utilitytests/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains utility tests for the Weather API.
+ * It includes classes and methods for building and asserting weather data for tests.
+ *
+ * @author Erik Wallenius
+ * @see <a href="https://github.com/knottem/WeatherApi">Repository Link</a>
+ */
+package com.example.weatherapi.utilitytests;

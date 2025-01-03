50c58162ca1a389e0454689b258445084a10385a
Erik Wallenius <walle.o.w@gmail.com>
2025-01-03 21:04:20 +0100

    sql query to get the latest valid weathers.

--- src/main/java/com/example/weatherapi/repositories/LatestWeatherApiRepository.java
+++ src/main/java/com/example/weatherapi/repositories/LatestWeatherApiRepository.java
@@ -3,12 +3,26 @@ package com.example.weatherapi.repositories;
 import com.example.weatherapi.domain.entities.CityEntity;
 import com.example.weatherapi.domain.entities.LatestWeatherApiEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface LatestWeatherApiRepository extends JpaRepository<LatestWeatherApiEntity, UUID> {
 
     Optional<LatestWeatherApiEntity> findByCityAndSmhiAndYrAndFmi(CityEntity city, boolean smhi, boolean yr, boolean fmi);
+
+    @Query("""
+    SELECT lwa
+    FROM LatestWeatherApiEntity lwa
+    JOIN FETCH lwa.latestWeather lw
+    JOIN FETCH lw.weatherDataList wd
+    WHERE lw.timeStamp > :validThreshold
+    """)
+    List<LatestWeatherApiEntity> findValidWeatherData(@Param("validThreshold") ZonedDateTime validThreshold);
+
 
 }

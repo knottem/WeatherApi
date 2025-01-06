c54669c499ac5500d38c76bd566c9e958e943cb9
Erik Wallenius <walle.o.w@gmail.com>
2025-01-06 17:19:44 +0100

    Removed unused query

--- src/main/java/com/example/weatherapi/repositories/ApiStatusRepository.java
+++ src/main/java/com/example/weatherapi/repositories/ApiStatusRepository.java
@@ -2,16 +2,13 @@ package com.example.weatherapi.repositories;
 
 import com.example.weatherapi.domain.entities.ApiStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Timestamp;
 import java.util.UUID;
 
 @Repository
 public interface ApiStatusRepository extends JpaRepository<ApiStatus, UUID> {
+
     ApiStatus findByApiName(String apiName);
 
-    @Query("SELECT MAX(a.lastChecked) FROM ApiStatus a")
-    Timestamp getLastUpdateTime();
 }
